4ecd1af3394c329448edcd53ae0f53cf2ba55879
Brendan Ribera <brendan.ribera@gmail.com>
2017-11-17 10:06:52 -0800

    nix/python noodling

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,14 +1,15 @@
 {
-  packageOverrides = super: let self = super.pkgs; in
-  {
-     myHaskellEnv =
-     self.haskellPackages.ghcWithPackages
-        (haskellPackages: with haskellPackages; [
-           # potentially add other haskell packages here
-           # mtl QuickCheck random text alex cpphs happy ghc-paths
-           # hfsevents zlib  yesod-test_1_4_4
-           yesod-bin
-        ]);
+  packageOverrides = pkgs: with pkgs; {
+     myPythonEnv = python36.withPackages
+       (ps: with ps; [ requests six ]);
+
+     myHaskellEnv = self.haskellPackages.ghcWithPackages
+       (haskellPackages: with haskellPackages; [
+         # potentially add other haskell packages here
+         # mtl QuickCheck random text alex cpphs happy ghc-paths
+         # hfsevents zlib  yesod-test_1_4_4
+         yesod-bin
+       ]);
   };
   allowUnfree = true;
 }
